bf607b12a766d883cc7ffa61f84bec7c9ee5abea
Lukas Hinsch <lukas@hinsch.eu>
2015-05-31 19:01:50 +0200

    Initial commit

---
+++ src/main/java/eu/hinsch/spring/boot/actuator/useragent/SpringBootActuatorUserAgentMetricsApplication.java
@@ -0,0 +1,32 @@
+package eu.hinsch.spring.boot.actuator.useragent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.embedded.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class SpringBootActuatorUserAgentMetricsApplication {
+
+    @Bean
+    public FilterRegistrationBean filter(UserAgentMetricFilter filter) {
+        FilterRegistrationBean bean = new FilterRegistrationBean();
+        bean.setFilter(filter);
+        bean.addUrlPatterns("/test");
+        return bean;
+    }
+
+    @RequestMapping("/tes")
+    @ResponseBody
+    public String test() {
+        return "ok";
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootActuatorUserAgentMetricsApplication.class, args);
+    }
+}
---
+++ src/main/java/eu/hinsch/spring/boot/actuator/useragent/UserAgentMetricFilter.java
@@ -0,0 +1,46 @@
+package eu.hinsch.spring.boot.actuator.useragent;
+
+import net.sf.uadetector.ReadableUserAgent;
+import net.sf.uadetector.service.UADetectorServiceFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.actuate.metrics.CounterService;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.*;
+import javax.servlet.http.HttpServletRequest;
+import java.io.IOException;
+
+/**
+ * Created by lh on 31/05/15.
+ */
+@Component
+public class UserAgentMetricFilter implements Filter {
+
+    private final CounterService counterService;
+
+    @Autowired
+    public UserAgentMetricFilter(CounterService counterService) {
+        this.counterService = counterService;
+    }
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+        if (request instanceof HttpServletRequest) {
+            HttpServletRequest httpServletRequest = (HttpServletRequest) request;
+
+            ReadableUserAgent userAgent = UADetectorServiceFactory.getResourceModuleParser().parse(httpServletRequest.getHeader("User-Agent"));
+            String key = userAgent.getName() + "." + userAgent.getVersionNumber().getMajor();
+            counterService.increment(key);
+        }
+    }
+
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+
+    }
+
+    @Override
+    public void destroy() {
+
+    }
+}
